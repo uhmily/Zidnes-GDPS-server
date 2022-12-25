31dcd6602151f2eddaecd957556354f91ef959b7
uhmily <depianoman2004@gmail.com>
2022-12-24 22:27:42 -0500

    Fix Daily Chests to not be so absurd

--- config/dailyChests.php
+++ config/dailyChests.php
@@ -1,27 +1,27 @@
 <?php
 /*
-	QUESTS
+        QUESTS
 */
 //NOW SET IN THE QUESTS TABLE IN THE MYSQL DATABASE
 /*
-	REWARDS
+        REWARDS
 */
 //SMALL CHEST
-$chest1minOrbs = 200;
-$chest1maxOrbs = 400;
-$chest1minDiamonds = 2;
-$chest1maxDiamonds = 10;
+$chest1minOrbs = 10;
+$chest1maxOrbs = 50;
+$chest1minDiamonds = 1;
+$chest1maxDiamonds = 3;
 $chest1items = [1, 2, 3, 4, 5, 6];
-$chest1minKeys = 1;
-$chest1maxKeys = 6;
+$chest1minKeys = 0;
+$chest1maxKeys = 1;
 //BIG CHEST
-$chest2minOrbs = 2000;
-$chest2maxOrbs = 4000;
-$chest2minDiamonds = 20;
-$chest2maxDiamonds = 100;
+$chest2minOrbs = 200;
+$chest2maxOrbs = 500;
+$chest2minDiamonds = 4;
+$chest2maxDiamonds = 6;
 $chest2items = [1, 2, 3, 4, 5, 6];
-$chest2minKeys = 1;
-$chest2maxKeys = 6;
+$chest2minKeys = 0;
+$chest2maxKeys = 1;
 //REWARD TIMES (in seconds)
 $chest1wait = 3600;
 $chest2wait = 14400;
