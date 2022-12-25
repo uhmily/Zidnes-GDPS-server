09b74612b88abf287cb3b743bff82952306a583f
uhmily <depianoman2004@gmail.com>
2022-12-24 22:35:08 -0500

    Update Credentials

--- config/connection.php
+++ config/connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "127.0.0.1";
 $port = 3306;
-$username = "root";
-$password = "";
-$dbname = "geometrydash";
+$username = $_ENV["MYSQL_GDPS_USER"];
+$password = $_ENV["MYSQL_GDPS_PASS"];
+$dbname = $_ENV["MYSQL_GDPS_NAME"];
 ?>
