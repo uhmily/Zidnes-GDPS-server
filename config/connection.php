96ffc76ff6c6c31b33ceb85dafc0c6a8b2f64fef
uhmily <depianoman2004@gmail.com>
2022-12-24 22:37:06 -0500

    Trying that again

--- config/connection.php
+++ config/connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "127.0.0.1";
 $port = 3306;
-$username = $_ENV["MYSQL_GDPS_USER"];
-$password = $_ENV["MYSQL_GDPS_PASS"];
-$dbname = $_ENV["MYSQL_GDPS_NAME"];
+$username = getenv("MYSQL_GDPS_USER");
+$password = getenv("MYSQL_GDPS_PASS");
+$dbname = getenv("MYSQL_GDPS_NAME");
 ?>
